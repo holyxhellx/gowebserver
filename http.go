9af835906322a9c5954f751c5f1eb7c3bb4b52b4
Mr. noName <noName@hotmail.com>
2016-08-19 12:53:44 +0000

    Added romanNoConvert 3

--- http.go
+++ http.go
@@ -15,6 +15,8 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func to_roman(n int)  string {
     if n == 2 {
         return "II"
+    } else if n == 3 {
+	return "III"	
     }
     return "I"
 }
